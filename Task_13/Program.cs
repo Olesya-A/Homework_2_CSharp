99fc847664ad10a5bc79e9abb819e5bd6b1e46ea
Olesya <lesusha1@yandex.ru>
2022-09-10 11:57:14 +0300

    Task_13

--- Task_13/Program.cs
+++ Task_13/Program.cs
@@ -16,3 +16,19 @@ else
 {
     Console.WriteLine($"Третья цифра числа = {input[2]}");
 }
+
+// Console.Write("Input any integer number: ");
+// int number = int.Parse(Console.ReadLine());
+// if (number > 100)
+// {
+//    while (number / 10 >= 100)
+//     {
+//         number = number / 10;
+//     }
+//    int remainder = number % 10;
+//    Console.WriteLine($"{remainder}");
+// }
+// else 
+//     {
+//         Console.WriteLine("There is no third digit");
+//    }
